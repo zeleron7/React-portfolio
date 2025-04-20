e132158502a068cfc4d7bfa8976dcfe234f663ee
zeleron7 <jessie.martensson@hotmail.com>
2025-04-20 23:23:31 +0200

    navmenu

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,6 @@ function App() {
                 </section>
             </>
           } />
-          <Route path="/" element={<></>} />
         </Routes>
       </Router>
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
+import navmenu from '../../assets/navmenu.png';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
     <h1 className={styles.title}>JM</h1>
     <div className={styles.menu}>
-      <ul className={styles.menuItems}>
+
+    <img src={navmenu} alt="navmenu" className={styles.navmenu} 
+    onClick={() => { setMenuOpen(!menuOpen); }}
+    />
+    
+      <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+      onClick={() => { setMenuOpen(false) }}
+      >
+
+
         {/*<li><a href="#projects">Projects</a></li>*/}
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
