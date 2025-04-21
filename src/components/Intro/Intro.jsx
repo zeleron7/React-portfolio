f6e30acd566d057be21e2ddf020f7a62a6c887c7
zeleron7 <jessie.martensson@hotmail.com>
2025-04-21 23:24:20 +0200

    responsive design

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -15,7 +15,7 @@ export const Intro = () => {
       <a href="#contact" className={styles.contact}>Contact</a>
 
       <p className={styles.description}>
-      I'm an upcoming .NET developer from a small town in Sweden, currently finishing my studies and set to graduate in end of May 2025. <br/>
+      I'm an upcoming .NET developer from a small town in Sweden, currently finishing my studies and set to graduate in end of May 2025. 
       I enjoy working behind the scenes on things like APIs and databases, and I'm especially interested in how different systems connect and communicate.
       </p>
       
